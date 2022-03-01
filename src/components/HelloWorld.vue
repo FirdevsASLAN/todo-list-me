a89d17ffe33944d44f9c5946d39d5e67f0470449
Firdevs Aslan <firdevsaslan6@gmail.com>
2022-03-01 14:28:43 +0300

    create todo item with icons and chcekbox

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,8 +3,22 @@
     <v-row class="text-center">
       <v-col class="mb-4">
         <h1 class="header__title display-2 font-weight-bold mb-3">Todo List</h1>
-        <v-text-field label="Solo" solo v-model="newTodo"></v-text-field>
+        <v-text-field label="new todo" solo v-model="newTodo"></v-text-field>
         <v-btn elevation="2" @click="addTodo">Add Todo</v-btn>
+        <div v-if="todoList.length > 0">
+          <div v-for="todoItem in todoList" :key="todoItem">
+            <v-checkbox
+              v-model="ex4"
+              color="red"
+              value="red"
+              hide-details
+            ></v-checkbox>
+            <span>{{ todoItem }}</span>
+            <v-icon> mdi-delete</v-icon>
+            <v-icon>mdi-content-save-outline</v-icon>
+            <v-icon> mdi-pencil-outline</v-icon>
+          </div>
+        </div>
       </v-col>
     </v-row>
   </v-container>
